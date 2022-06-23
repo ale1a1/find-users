c1ac09dc2f308b01675dfa32367b97d221e17da6
ale1a1 <68277684+ale1a1@users.noreply.github.com>
2022-06-23 09:36:29 +0100

    ...

--- client/src/components/MainPage/MainPage.js
+++ client/src/components/MainPage/MainPage.js
@@ -1,58 +1,66 @@
 import { Fragment } from "react";
 
+import "../../style.css";
+
 export function MainPage(props) {
   return (
     <Fragment>
-      <div className="container pt-5">
-        <h3 className="text-light mt-5 mb-5">FIND USERS WEB APPLICATION</h3>
-        <ul>
-          <h5 className="text-light mt-3">
-            Check the
-            <button className="btn btn-sm">
-              <a
-                className="nav-link link-warning text-uppercase h5"
-                href="users"
-              >
-                Users List
-              </a>
-            </button>
-          </h5>
-          <h5 className="text-light mt-3">
-            Retrieve
-            <button className="btn  btn-sm">
-              <a
-                className="nav-link link-warning text-uppercase h5"
-                href="users"
-              >
-                Users
-              </a>
-            </button>
-            informations
-          </h5>
-          <h5 className="text-light mt-3">
-            Save your
-            <button className="btn  btn-sm">
-              <a
-                className="nav-link link-warning text-uppercase h5"
-                href="favourites"
-              >
-                favourite Users
-              </a>
-            </button>
-            and access to their contact details at any time
-          </h5>
-          <h5 className="text-light mt-3">
-            Update
-            <button className="btn  btn-sm">
-              <a
-                className="nav-link link-warning text-uppercase h5"
-                href="profile"
-              >
-                your profile details
-              </a>
-            </button>
-            at any time
-          </h5>
+      <div className="container text-light pt-5">
+        <h3 className="mt-5 mb-5">FIND USERS WEB APPLICATION</h3>
+        <ul className="highMarginTop">
+          <li>
+            <h5 className="mt-3">
+              Check the
+              <button className="btn btn-sm">
+                <a
+                  className="nav-link link-warning text-uppercase h5"
+                  href="users"
+                >
+                  Users List
+                </a>
+              </button>
+            </h5>
+          </li>
+          <li>
+            <h5 className="mt-3">
+              Retrieve
+              <button className="btn  btn-sm">
+                <a
+                  className="nav-link link-warning text-uppercase h5"
+                  href="users"
+                >
+                  Users
+                </a>
+              </button>
+              informations
+            </h5>
+          </li>
+          <li>
+            <h5 className="mt-3">
+              Save your
+              <button className="btn  btn-sm">
+                <a
+                  className="nav-link link-warning text-uppercase h5"
+                  href="favourites"
+                >
+                  favourite Users
+                </a>
+              </button>
+            </h5>
+          </li>
+          <li>
+            <h5 className="mt-3">
+              Update
+              <button className="btn  btn-sm">
+                <a
+                  className="nav-link link-warning text-uppercase h5"
+                  href="profile"
+                >
+                  your profile details
+                </a>
+              </button>
+            </h5>
+          </li>
         </ul>
       </div>
     </Fragment>

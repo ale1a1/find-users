24be36c33a157aef6e4077bdde527139410134ec
ale1a1 <68277684+ale1a1@users.noreply.github.com>
2022-07-31 15:23:02 +0100

    ...

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../../style.css";
 
 export function Footer() {
-  const yearEl = document.querySelector(".year");
+  const yearEl = document.querySelector("#year");
   const currentYear = new Date().getFullYear();
   yearEl.textContent = currentYear;
 
@@ -18,7 +18,7 @@ export function Footer() {
     >
       <div className="row align-items-center px-3">
         <h2 className="col smallFontSize mt-2 mb-0 ms-xs-auto ms-sm-auto ms-lg-2 footerH2 font-monospace">
-          &copy; Copyright <span className="year">20??</span>
+          &copy; Copyright <span id="year">20??</span>
           <a
             className="text-decoration-none text-light ms-3"
             href="https://portfolio-ale1a184.herokuapp.com/#home"

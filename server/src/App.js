c31a94cc58f48489a08d49843280dce82a214667
ale1a1 <68277684+ale1a1@users.noreply.github.com>
2023-10-04 20:29:09 +0100

    reverted last changes

--- server/src/App.js
+++ server/src/App.js
@@ -23,11 +23,4 @@ module.exports = () => {
   app.listen(process.env.PORT || PORT, () =>
     console.log("API is running on http://localhost:3001")
   );
-
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://find-users-app.netlify.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
 };
